05bbf8efe028d93f135ca45f3866ffc6d352008a
Aaron Marks <aaron.marks@cdpehs.com>
2020-05-18 18:15:33 -0400

    Adding generic tests

---
+++ test/example.test.js
@@ -0,0 +1,14 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const describe = mocha.describe;
+const it = mocha.it;
+
+describe('Simple Math Test', () => {
+  it('should return 2', () => {
+    assert.strictEqual(1 + 1, 2);
+  });
+
+  it('should return 9', () => {
+    assert.strictEqual(3 * 3, 9);
+  });
+ });
